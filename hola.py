f9ef4216cb5f1f31d57d91241b4192643c5b6435
Luis Jose Mejia Ramos <ljmr9807@gmail.com>
2024-04-25 13:17:47 -0600

    comiits

--- hola.py
+++ hola.py
@@ -1,11 +1,2 @@
-print("hola mundo")
-
-print("hola soy luis")
-
-x = 5
-y = 2
-
-if(x > y):
-    print("El mayor es ", x)
-else:
-    print("El mayor es ", y)
+for i in [1, 2, 3, 4]:
+    print(i, end=", ") # prints: 1, 2, 3, 4,
